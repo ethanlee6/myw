0a33e83fb6d37fcbe585d8a6edc1a1831d4512cd
lg_online <lg_online@hotmail.com>
2017-03-10 01:16:21 +0800

    Initial commit

---
+++ webapp/forms.py
@@ -0,0 +1,61 @@
+from flask_wtf import Form,RecaptchaField
+from wtforms import StringField,PasswordField,BooleanField,TextAreaField
+from wtforms.validators import DataRequired,Length,URL,EqualTo
+from models import User
+
+
+class CommentForm(Form):
+    name = StringField(
+        'Name',
+        validators=[DataRequired(), Length(max=255)]
+    )
+    text = TextAreaField(u'Comment', validators=[DataRequired()])
+
+class PostForm(Form):
+    title = StringField('Title',[DataRequired(),Length(max=255)])
+    text = TextAreaField('Content',[DataRequired()])
+
+class LoginForm(Form):
+    username = StringField('Username',[DataRequired(),Length(max=255)])
+    password = PasswordField('Password',[DataRequired()])
+    remember = BooleanField('Remember Me')
+
+    def validate(self):
+        check_validate = super(LoginForm,self).validate()
+
+        if not check_validate:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+
+        if not user:
+            self.username.errors.append('Invalid username or password')
+            return False
+
+        if not user.check_password(self.password.data):
+            self.username.errors.append('Invalid username or password')
+            return False
+
+        return True
+
+class OpenIDForm(Form):
+    openid = StringField('OpenID URL', [DataRequired(), URL()])
+
+class RegisterForm(Form):
+    username = StringField('Username',[DataRequired(),Length(max=255)])
+    password = PasswordField('Password',[DataRequired(),Length(max=255)])
+    confirm = PasswordField('Confirm Password',[DataRequired(),EqualTo('password')])
+    recapacha = RecaptchaField()
+
+    def validate(self):
+        check_validate = super(RegisterForm,self).validate()
+
+        if not check_validate:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+
+        if user:
+            self.username.errors.append('User with that name already exists')
+
+        return True
